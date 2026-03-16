bd90b19fe27d238d35c22ba905772d2d243fc244
Weronika Kowalska <weronika.kowalskaa@op.pl>
2026-03-16 20:01:56 +0100

    build to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/portfolio",
+  base: "/portfolio/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
 })
